f85ccc5e5ff7e03c089c9d9c47cd8dcb9e6d664b
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-09-09 12:10:43 -0500

    added to the external.js file

--- js/external.js
+++ js/external.js
@@ -2,4 +2,5 @@
 console.log("Hello from external js!");
 alert("Welcome to my Website!");
 prompt("What is your favorite color?");
-alert("Great, ")
+let favColor = prompt("What is your favorite color?");
+alert("Awesome, my favorite color is " + favColor + " too.")
