64fde03bd664ae0cb8a3f9716957a4bfdd2cfba8
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-09-09 13:02:43 -0500

    Finished the first two questions of the first HTML exercise

--- js/external.js
+++ js/external.js
@@ -1,6 +1,26 @@
 "use strict";
 console.log("Hello from external js!");
 alert("Welcome to my Website!");
-prompt("What is your favorite color?");
 let favColor = prompt("What is your favorite color?");
 alert("Awesome, my favorite color is " + favColor + " too.")
+let rentedMovies = prompt("how many movies did you rent?")
+let daysRented = prompt("How many days did you have movies rented?")
+let priceOfRentals = (daysRented * 3)
+alert("That comes out to " + priceOfRentals + "$")
+alert("I hear you are a contractor, lets calculate your pay!")
+let firstCompany = prompt("What is the first company you work with?")
+let secondCompany = prompt("And the second company?")
+let thirdCompany = prompt("finally the third?")
+let firstPay = prompt("How much does " + firstCompany + " pay you per hour?")
+let secondPay = prompt("How much does " + secondCompany + " pay you per hour?")
+let thirdPay = prompt("How much does " + thirdCompany + " pay you per hour?")
+let firstTotal = prompt("How many hours at " + firstCompany + "?") * firstPay
+let secondTotal = prompt("How many hours at " + secondCompany + "?") * secondPay
+let thirdTotal = prompt("Finally! How many at " + thirdCompany + "?") * thirdPay
+let totalPay = firstTotal + secondTotal + thirdTotal
+alert("You get paid " + totalPay + "$!")
+
+
+
+
+
