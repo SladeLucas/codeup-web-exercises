6d6c533fe09ff97f632c86222910891a939eaa7c
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-09-18 07:22:39 -0500

    finished with the break and continue questions and tested successfully

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,13 +1,18 @@
 (function(){
 'use strict';
-    for (let i = 1; i < 50; i++) {
-        let x = prompt("Enter an odd number between 1 and 50");
-        if (i % 2 !== 0) {
-            continue;
-        }
-        alert("That wasn't an odd number...")
+    let odd;
+    while(1) {
+        odd = prompt("Enter an odd number between 1 and 50")
+        if(odd % 2 !== 0) break;
     }
-console.log("sanity?")
+    for(let i = 0; i < 50;i++){
+        if(i == odd) {
+            console.log(`Yikes! Skipping number: ${i}`);
+            continue;
+        }else if(i % 2 !== 0){
+            console.log(`Here is an odd number: ${i}`);
+        }
+    }
 
 
 })();
