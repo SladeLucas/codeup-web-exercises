8441921b4797c7b67d07794414a383ad14a2db53
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-09-21 13:14:34 -0500

    finished and tested the circle.js exercise of the topic objects

--- js/circle.js
+++ js/circle.js
@@ -3,22 +3,25 @@
 
     // create a circle object
     var circle = {
-        radius: 3,
+        radius: 5,
 
         getArea: function () {
             // TODO: complete this method
-            // hint: area = pi * radius^2
+            let area = Math.PI * Math.pow(this.radius, 2);
 
-            return; // TODO: return the proper value
+            return area // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            if (doRounding == true){
+                return console.log(Math.round(this.getArea()));
 
+            }else {
+                return console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
         }
     };
 
@@ -27,10 +30,10 @@
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
-
+    //Radius of 3 returned 28.stuff and 28 when rounded.
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    // Didn't say you need both outcomes to read at the same time!
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
