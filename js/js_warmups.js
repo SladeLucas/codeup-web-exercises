a493fb9d034796723558c973a409cd787ca0edaa
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-09-26 11:31:01 -0500

    added some styles via a stylesheet to the welcome.html. things are getting pretty

---
+++ js/js_warmups.js
@@ -0,0 +1,20 @@
+(function(){
+    'use strict'
+    function arrayEven(array){
+        let even = array.filter(array => {
+            return array % 2 === 0;
+        });
+    }
+
+    arrayEven(even);
+
+
+
+
+
+
+
+
+
+
+})();
