20b9ea6dd3a9118d40d841f33d11ec571e2cd143
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-10-20 09:18:09 -0500

    added an event to change h1

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,7 @@
 "use strict";
 
-$('li').css('font-size', '20px');
-
-$('h1, p, li').css('background-color', '#FF0');
-
-let content = $('h1').html();
-alert(content);
+$('h1').click(
+    function () {
+        $(this).css('background-color', '#FF0');
+    }
+)
