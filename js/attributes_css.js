4dd94a6c801e530c50d98a77709e0b6b95d93fd8
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-10-21 10:59:07 -0500

    added a clickable function to make FAQs appear

--- js/attributes_css.js
+++ js/attributes_css.js
@@ -1,4 +1,23 @@
 "use strict";
 
-$('.invisible').css('visibility', 'hidden');
 
+$('.invisible').css({
+    'visibility': 'hidden'
+});
+
+$('dd').addClass('invisible');
+
+$(document).ready(function() {
+    $('p').click(function(event) {
+        event.preventDefault();
+        $('dd').toggleClass('important');
+        $('dd').toggleClass('invisible');
+    });
+});
+
+
+//$('.important').css({
+//        "color": 'red',
+//        'visibility': 'visible'
+//    }
+//);
