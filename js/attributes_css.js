cb85410652f71b1f320d52cfd416ba327f98f50f
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-10-21 13:47:58 -0500

    added a clickable event to h3 allowed it to bold the li

--- js/attributes_css.js
+++ js/attributes_css.js
@@ -1,32 +1,38 @@
 "use strict";
 
 
-$('.invisible').css({
-    'visibility': 'hidden'
-});
+//$('.invisible').css({
+//    'visibility': 'hidden'
+//});
+//
+//$('dd').addClass('invisible');
 
-$('dd').addClass('invisible');
-
-$(document).ready(function() {
-    $('p').click(function(event) {
-        event.preventDefault();
-        $('dd').toggleClass('important');
-        $('dd').toggleClass('invisible');
-    });
-});
-
-$(document).ready(function() {
-    $('dt').click(function(event) {
-        event.preventDefault();
-        $('dd').toggleClass('highlighted');
-        $('body').css('background-color', 'yellow');
-    });
-});
+//$(document).ready(function() {
+//    $('p').click(function(event) {
+//        event.preventDefault();
+//        $('dd').toggleClass('important');
+//        $('dd').toggleClass('invisible');
+//    });
+//});
+//
+//$(document).ready(function() {
+//    $('dt').click(function(event) {
+//        event.preventDefault();
+//        $('dd').toggleClass('highlighted');
+//        $('body').css('background-color', 'yellow');
+//    });
+//});
 //$('.important').css({
 //        "color": 'red',
 //        'visibility': 'visible'
 //    }
 //);
+$(document).ready(function() {
+    $('h3').click(function(e) {
+        e.preventDefault();
+        $(this).next().children().css('font-weight', 'bold');
+    });
+});
 
 $("#highlighter").click(function(e){
     $('li').each( function (index) {
