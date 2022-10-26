c84b2e565c07511cf938579eba24744ef8f08c68
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-10-26 12:18:54 -0500

    adding in some js

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,2 +1,18 @@
 "use strict";
+
 //calling the api
+const url = 'http://api.openweathermap.org/data/2.5/weather';
+$.get(url, {
+    APPID: MY_WEATHER_ID,
+    q:     "Salt Lake City, US"
+}).done(function(data) {
+    console.log(data);
+});
+
+function currentWea(obj){
+    let weatherNow = "";
+    let condition = obj.currently;
+    let temp = parseInt(obj.currently.temperature);
+    weatherNow += '<div class="card"> Current temperature: " + temp + "°F</div>';
+
+}
