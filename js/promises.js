6fb8952b6ae87bb2185826c05c63f4bf7d00b708
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-01 15:12:28 -0600

    added a function that returns users last commit, refactoring

--- js/promises.js
+++ js/promises.js
@@ -9,3 +9,15 @@ function wait(num) {
 };
 //wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 //wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+function userLastCommit(username) {
+    let commit = '';
+    fetch("https://api.github.com/users/" + username + "/events", {headers: {'Authorization': "token " + MY_GITHUB_KEY}})
+        .then((response) => {
+            response.json().then((events) => {
+                commit = (events[0].created_at);
+                return commit;
+            }).then(commit => console.log(`${username}'s last commit: ${commit}`))
+        }).catch((err) => console.log(err));
+}
+userLastCommit('SladeLucas')
